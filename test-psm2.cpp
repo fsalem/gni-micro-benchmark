22df3f36674a95ed3600d706bde9811126858d50
Farouk Salem <salem@zib.de>
2020-06-09 01:23:43 +0200

    bug fix

--- test-psm2.cpp
+++ test-psm2.cpp
@@ -281,7 +281,7 @@ int main(int argc, char **argv) {
 				}
 			}
 			if ((event.flags & FI_MSG) != 0) {
-				std::cout << "FI_MSG " << std::endl;
+				std::cout << "FI_MSG " << counter << std::endl;
 				//std::cout << "FI_MSG " << event.flags << std::endl;
 				//std::cout << "FI_MSG " << event.buf << std::endl;
 				struct mr_message *msg;
